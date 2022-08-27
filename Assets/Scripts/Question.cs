68e308abe9277a3fc5aa7ad7fb402d942ba1adde
Trece-1101 <omar.bazzi.sf@gmail.com>
2022-08-27 12:20:04 -0300

    answers for the questions

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+
 public class Question : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] private string questionText = "Question Text";
+    [SerializeField] private string[] answers = new string[4];
+    [SerializeField][Range(0, 3)] private int correctAnswerIndex = 0;
 
-    public string QuestionText { get => questionText; set => questionText = value; }
+    public string QuestionText { get => questionText; }
+    public string[] Answers { get => answers; }
+    public string Answer(int index) { return Answers[index]; }   
+    public int CorrectAnswerIndex { get => correctAnswerIndex; }
 }
