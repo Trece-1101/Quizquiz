6e7fb01ffdb2525a5574e4e6a953ee891aaa9f1c
Trece-1101 <omar.bazzi.sf@gmail.com>
2022-08-28 12:21:57 -0300

    Pie timer and text timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private float timeForAnswer = 20.0f;
+    [SerializeField] private float timeLeftForAnswer = 20.0f;
+    private float originalTimeForAnswer;
     private bool isAnswerSelected = false;
     private bool isTimeLeft = true;
+    private float timerFraction;
+    private TextMeshProUGUI timeLeftText;
+    private Image quizTimer;
     private Quiz quiz;
 
     private void OnEnable()
@@ -18,15 +24,16 @@ public class Timer : MonoBehaviour
     {
         Quiz.AnswerClicked -= AnswerSelected;
     }
-
-
-    private void AnswerSelected()
-    {
-        isAnswerSelected = true;
-    }
     private void Awake()
     {
         quiz = FindObjectOfType<Quiz>();
+        quizTimer = GetComponent<Image>();
+        timeLeftText = transform.Find("TimeLeft").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        originalTimeForAnswer = timeLeftForAnswer;
     }
     private void Update()
     {
@@ -34,19 +41,43 @@ public class Timer : MonoBehaviour
         UpdateTimer();
     }
 
+    private void AnswerSelected()
+    {
+        isAnswerSelected = true;
+        ResetTimer();
+    }
+
     private void UpdateTimer()
     {
-        timeForAnswer -= Time.deltaTime;
-        // cambiar valor numero timer solo cuando es segundo exacto...
+        timeLeftForAnswer -= Time.deltaTime;
+        UpdateVisual();
+        CheckTimeLeft();
+    }
 
-        if (timeForAnswer <= float.Epsilon)
+    private void UpdateVisual()
+    {
+        // originalTimeForAnswer -- 100% (1.0) --> timeLeftForAnswer -- X% (x)
+        // X = timeLeftForAnswer * 1.0 / originalTimeForAnswer --> timeLeftForAnswer / originalTimeForAnswer
+        timerFraction = timeLeftForAnswer / originalTimeForAnswer;
+        quizTimer.fillAmount = 1f - timerFraction;
+
+        var roundTime = Mathf.Ceil(timeLeftForAnswer);
+        timeLeftText.text = $"{roundTime}";
+    }
+
+    private void CheckTimeLeft()
+    {
+        if (timeLeftForAnswer <= float.Epsilon)
         {
             // Siguiente pregunta
             quiz.AnswerDisplay(false, false);
             Debug.Log("perdiste");
             isTimeLeft = false;
         }
+    }
 
-        Debug.Log(timeForAnswer);
+    private void ResetTimer()
+    {
+        timeLeftForAnswer = originalTimeForAnswer;
     }
 }
